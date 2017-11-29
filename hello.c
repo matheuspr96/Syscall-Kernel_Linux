7bddc7832def311ddf2a0dbd87def3b01e39a6df
matheuspr <matheuspr96@hotmail.com>
2017-11-29 09:55:52 -0200

    mlk bagui eh o siginte sepeu time ta pronto e o return -1 tb

--- hello.c
+++ hello.c
@@ -3,42 +3,25 @@
 #include <linux/module.h>
 #include <linux/pid.h>		//pid_task(), PIDTYPE_PID
 #include <linux/sched.h>
+#include <linux/sched/cputime.h>
 
-/*Antigo
-extern struct task_struct *find_task_by_vpid(pid_t nr);
-EXPORT_SYMBOL(find_task_by_vpid);
-extern struct task_struct *pid_task(struct pid *pid, enum pid_type);
-Antigo//
-extern struct task_struct *pid_task(struct pid *pid, enum pid_type);
-extern struct task_struct *get_pid_task(struct pid *pid, enum pid_type);
-//////agora eu não sei qual das 2, mas o kra do fórum mandou usar a pid_task()
-*/
+//long int tempo_tarefa = task_sched_runtime(task); // Deu erro ao compilar e o propio compilador recomendou o uso
+//long int tempo_CPU = task->se.sum_exec_runtime;   Funciona porem chama função 
 
-// A alterar
 
 asmlinkage long sys_hello(int pid){
-	//unsigned long long int exec_time;
-	
+
 	struct task_struct *task = NULL;
 	task  = pid_task(find_vpid(pid), PIDTYPE_PID);
 	
-	
-	//if(task->pid == NULL) 
-		//return -1;
-						
-	//exec_time = task -> cputime_expires.sum_exec_runtime;
-	//task -> shed_info.pcount;
+	if(task == NULL) 
+		return -1;
 
-	
 	printk("O pid eh %d\n",task->pid);
 	printk("Quantidade de vezes que o processo %d passa pela CPU: %ld\n",task->pid ,(unsigned long int)task->sched_info.pcount);
-	printk("Tempo que o processo ficou na CPU : %ld\n", (unsigned long int)(task->utime + task->stime));
+	printk("Time CPU: %ld\n",(unsigned long int)task->se.sum_exec_runtime);
 
-	//printk("Usando ponteiro %lld\n",task -> cputime_expires->sum_exec_runtime);
-
-/*Antigo
-	struct task_struct *processo = find_task_by_vpid(pid);
-	printk("o nr do processo eh %d\n",task_pid_nr(processo));
-Antigo*/
+	printk("Tempo inicio processo: %ld\n", task->start_time);
+	
 	return 0;	
 }
