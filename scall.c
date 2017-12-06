2da385b7977500d97c2a1c309a9f07df67e4f0f8
João Paulo <joaopaulodeoliveira123@gmail.com>
2017-12-06 14:31:50 -0200

    agora funciona até mesmo com a passagem por referência

--- scall.c
+++ scall.c
@@ -1,28 +1,30 @@
 #include <linux/linkage.h>	//Syscall espera argumento da pilha
 #include <linux/kernel.h>	//Printk
-#include <linux/module.h>
+//#include <linux/module.h>
 #include <linux/pid.h>		//pid_task(), PIDTYPE_PID
 #include <linux/sched.h>
-#include <linux/sched/cputime.h>
-#include <linux/time.h>
+#include <linux/uaccess.h>
+//#include <linux/sched/cputime.h>
+//#include <linux/time.h>
 #include <linux/timekeeping.h>
 
-asmlinkage long sys_det(int pid){
-	struct timespec bott;
+asmlinkage long sys_det(int pid, long int *n_in_CPU, long int *CPU_time, long int *lifetime){
 	struct task_struct *task = NULL;
-	struct system_time_snapshot snap;
+	long int life;
 	task  = pid_task(find_vpid(pid), PIDTYPE_PID);
 
 	if(task == NULL) 
 		return -1;
-	
-	getboottime(&bott);
-
 	printk("O pid eh %d\n",task->pid);
-	printk("Quantidade de vezes que o processo %d passa pela CPU: %ld\n",task->pid ,(unsigned long int)task->sched_info.pcount);
-	printk("Time CPU: %ld\n",(unsigned long int)task->se.sum_exec_runtime);
-	ktime_get_snapshot(&snap);
-	//printk("oq era pra ser a hora atual: %lld\n",snap.real);
-	printk("Tempo de vida do processo: %lld s\n",(snap.real - (task->real_start_time+bott.tv_nsec))/1000000000);
-	return 0;	
+	life = ktime_to_timespec(ktime_get_boottime()).tv_nsec - task->start_time;
+	if(copy_to_user(n_in_CPU ,&(task->sched_info.pcount),sizeof(long int)))
+		return -1;
+	if(copy_to_user(CPU_time ,&(task->se.sum_exec_runtime),sizeof(long int)));
+		return -1;
+	if(copy_to_user(lifetime ,&life,sizeof(long int)));
+		return -1;
+	printk("Quantidade de vezes que o processo %d passa pela CPU: %ld\n",task->pid ,*n_in_CPU);
+	printk("Time CPU: %ld\n",*CPU_time);
+	printk("Tempo de vida do processo: %ld s\n", *lifetime);
+	return 0;
 }
--- scall.c~
+++ scall.c~
@@ -8,21 +8,21 @@
 #include <linux/timekeeping.h>
 
 asmlinkage long sys_det(int pid){
-
+	struct timespec bott;
 	struct task_struct *task = NULL;
+	struct system_time_snapshot snap;
 	task  = pid_task(find_vpid(pid), PIDTYPE_PID);
 
 	if(task == NULL) 
 		return -1;
-	struct timespec bott;
+	
 	getboottime(&bott);
 
-	/*
 	printk("O pid eh %d\n",task->pid);
 	printk("Quantidade de vezes que o processo %d passa pela CPU: %ld\n",task->pid ,(unsigned long int)task->sched_info.pcount);
 	printk("Time CPU: %ld\n",(unsigned long int)task->se.sum_exec_runtime);
-*/
-	printk("start_time: %lld em segundos\n\n", (task->start_time/1000000000));
-	printk("boot_time: %lld em segundos\n\n", bott.tv_sec);
+	ktime_get_snapshot(&snap);
+	//printk("oq era pra ser a hora atual: %lld\n",snap.real);
+	printk("Tempo de vida do processo: %lld s\n",(snap.real - (task->real_start_time+bott.tv_nsec))/1000000000);
 	return 0;	
 }
